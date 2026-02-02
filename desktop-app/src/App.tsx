36c17cb45615727edaedcc21fbb72ca8a80cfb66
hariskillprompt <hari.skillprompt@gmail.com>
2026-02-02 10:46:26 +0545

    First Commit

---
+++ desktop-app/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-red">Hari Bhandari</h1>
+        <p>This is my first desktop app.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
